b854b35743b5f81c3e0ded174428eb7683ebea87
joziel <candav09@gmail.com>
2022-08-31 22:06:45 -0600

    revision

--- JS.js
+++ JS.js
@@ -1,3 +1,29 @@
 (function(){
-const sliders = document.querySelectorAll(".test_body")
+const sliders = [...document.querySelectorAll(".test_body")];
+
+const buttonNext = document.querySelector("#next");
+
+const buttonBefore = document.querySelector("#before");
+
+let value;
+buttonNext.addEventListener("click", ()=>{
+changePosition(1);
+});
+
+buttonBefore.addEventListener("click", ()=>{
+    changePosition(-1);
+});
+
+const changePosition = (add)=>{
+const currentTest = document.querySelector(".test_body--show").dataset.id;
+value = Number(currentTest);
+value+= add;
+
+sliders[Number(currentTest)-1].classList.remove('test__body--show');
+if(value === sliders.length+1 || value == 0 ){
+    value = value === 0 ? sliders.length :  1;
+}
+sliders[value-1].classList.add('test__body--show');
+}
+
 })();
